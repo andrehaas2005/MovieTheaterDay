9b179d13564f1eb0ccd5135162d7010943ff075f
Andre Haas <andrehaas2005@gmail.com>
2024-06-25 14:15:15 -0300

    Initial Commit

---
+++ Domain/Models/AccountModel.swift
@@ -0,0 +1,8 @@
+//
+//  AccountModel.swift
+//  Domain
+//
+//  Created by Andre  Haas on 25/06/24.
+//
+
+import Foundation
---
+++ Domain/UseCases/AddAccount.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+protocol AddAccount {
+    func add(addAccountModel: AddAccountModel, completion: @escaping (Result<AccountModel, Error>) -> Void)
+}
+
+public struct AddAccountModel: Model {
+    public var name: String
+    public var email: String
+    public var password: String
+    public var passwordConfirmation: String
+
+   public init(name: String, email: String, password: String, passwordConfirmation: String) {
+        self.name = name
+        self.email = email
+        self.password = password
+        self.passwordConfirmation = passwordConfirmation
+    }
+}
+struct AccountModel {
+    
+}

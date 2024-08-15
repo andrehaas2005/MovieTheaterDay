837d3196948b4a646351df7679bd560718f488ab
Andre Haas <andrehaas2005@gmail.com>
2024-08-15 10:57:54 -0300

    mudando versao iOS e macOS

---
+++ MovieTheaterDay/MovieTheaterDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieTheaterDayApp.swift
+//  MovieTheaterDay
+//
+//  Created by Andre  Haas on 31/07/24.
+//
+
+import SwiftUI
+
+@main
+struct MovieTheaterDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
